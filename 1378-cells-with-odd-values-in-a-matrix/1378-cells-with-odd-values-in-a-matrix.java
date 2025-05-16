50f7a2a0295787b119416ac200afd5063f65b605
Bojja Sanjeev Kumar <bojjasanjeev555@gmail.com>
2025-05-16 09:52:29 +0530

    Time: 1 ms (87.21%), Space: 41.6 MB (78.36%) - LeetHub

--- 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.java
+++ 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.java
@@ -9,7 +9,7 @@ class Solution {
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if((rows[i]+cols[j]) % 2 ==1)
+                if((rows[i]+cols[j]) % 2 !=0)
                 count++;
             }
         }
