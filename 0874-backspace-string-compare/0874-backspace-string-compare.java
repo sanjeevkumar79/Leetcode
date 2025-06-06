590133def9cbc2afcb6a1b8a8fbe571cc4c42729
Bojja Sanjeev Kumar <bojjasanjeev555@gmail.com>
2025-06-06 09:10:07 +0530

    Time: 1 ms (77.28%), Space: 41.4 MB (91.22%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.java
+++ 0874-backspace-string-compare/0874-backspace-string-compare.java
@@ -1,54 +1,54 @@
-class Solution {
-    public boolean backspaceCompare(String s, String t) {
-        Stack<Character> stack1 = new Stack<>();
-        Stack<Character> stack2 = new Stack<>();
-        for(int i = 0; i<s.length(); i++){
-            if(s.charAt(i)!='#'){
-                stack1.push(s.charAt(i));
-            }
-            else{
-                if(!stack1.isEmpty()){
-                    stack1.pop();
-                }
-            }
-        }
-        for(int i = 0; i<t.length(); i++){
-            if(t.charAt(i)!='#'){
-                stack2.push(t.charAt(i));
-            }
-            else{
-                if(!stack2.isEmpty()){
-                    stack2.pop();
-                }
-            }
-        }
-        if(!stack1.equals(stack2))
-            return false;
-        return true;
-    }
-}
-
-
 // class Solution {
 //     public boolean backspaceCompare(String s, String t) {
-//         return buildString(s).equals(buildString(t));  
-//     }
-
-//     private String buildString(String str){
-//         Stack<Character> stack = new Stack();
-//         for(char ch : str.toCharArray()){
-//             if(ch != '#'){
-//                 stack.push(ch);
+//         Stack<Character> stack1 = new Stack<>();
+//         Stack<Character> stack2 = new Stack<>();
+//         for(int i = 0; i<s.length(); i++){
+//             if(s.charAt(i)!='#'){
+//                 stack1.push(s.charAt(i));
 //             }
-//             else if(!stack.isEmpty()){
-//                 stack.pop();
+//             else{
+//                 if(!stack1.isEmpty()){
+//                     stack1.pop();
+//                 }
 //             }
 //         }
-
-//         StringBuilder res = new StringBuilder();
-//         for(char c : stack){
-//             res.append(c);
+//         for(int i = 0; i<t.length(); i++){
+//             if(t.charAt(i)!='#'){
+//                 stack2.push(t.charAt(i));
+//             }
+//             else{
+//                 if(!stack2.isEmpty()){
+//                     stack2.pop();
+//                 }
+//             }
 //         }
-//         return res.toString();
+//         if(!stack1.equals(stack2))
+//             return false;
+//         return true;
 //     }
 // }
+
+
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return buildString(s).equals(buildString(t));  
+    }
+
+    private String buildString(String str){
+        Stack<Character> stack = new Stack();
+        for(char ch : str.toCharArray()){
+            if(ch != '#'){
+                stack.push(ch);
+            }
+            else if(!stack.isEmpty()){
+                stack.pop();
+            }
+        }
+
+        StringBuilder res = new StringBuilder();
+        for(char c : stack){
+            res.append(c);
+        }
+        return res.toString();
+    }
+}
